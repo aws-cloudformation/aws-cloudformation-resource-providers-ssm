d1b874769dd6b5a1ace86b5dd7dff67150fbc9c8
Gowtam Reddy <gowtamm@amazon.com>
2020-03-04 16:17:07 -0800

    Create resource handler implementation for SSM Document resource (#12)
    * createHandler, resource schema and boiler plate code for CRUDL Handlers

---
+++ aws-ssm-document/src/main/java/com/amazonaws/ssm/document/CallbackContext.java
@@ -0,0 +1,22 @@
+package com.amazonaws.ssm.document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonDeserialize(builder = CallbackContext.CallbackContextBuilder.class)
+public class CallbackContext {
+
+    private Boolean createDocumentStarted;
+
+    private Integer stabilizationRetriesRemaining;
+
+    @JsonIgnore
+    public void decrementStabilizationRetriesRemaining() {
+        stabilizationRetriesRemaining--;
+    }
+}
