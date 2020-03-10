72f0e737678476c8e38b766eca1b720994a95ce0
Narasimha <nbharad@amazon.com>
2020-03-09 23:51:48 -0700

    Init setup

---
+++ aws-ssm-parameter/src/main/java/software/amazon/ssm/parameter/CreateHandler.java
@@ -0,0 +1,36 @@
+package software.amazon.ssm.parameter;
+
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+import software.amazon.cloudformation.resource.IdentifierUtils;
+
+public class CreateHandler extends BaseHandler<CallbackContext> {
+
+    @Override
+    public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final Logger logger) {
+
+        final ResourceModel model = request.getDesiredResourceState();
+
+        // Set model primary ID if absent
+        if(model.getId() == null) {
+            model.setId(IdentifierUtils.generateResourceIdentifier(
+                request.getLogicalResourceIdentifier(),
+                request.getClientRequestToken()
+            ));
+        }
+
+        // TODO : put your code here
+
+        return ProgressEvent.<ResourceModel, CallbackContext>builder()
+            .resourceModel(model)
+            .status(OperationStatus.SUCCESS)
+            .build();
+    }
+}
