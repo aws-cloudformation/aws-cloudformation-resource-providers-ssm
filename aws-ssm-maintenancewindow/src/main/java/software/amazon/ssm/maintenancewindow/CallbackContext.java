7849edeeffeba417d091b443377d622e7c7ebecc
nickysincreate <nicky.singh2008@gmail.com>
2020-04-10 17:52:41 -0700

    Init setup (#22)
    * Init setup
    
    * Init setup
    
    Co-authored-by: Nicky Singh <snicky@amazon.com>

---
+++ aws-ssm-maintenancewindow/src/main/java/software/amazon/ssm/maintenancewindow/CallbackContext.java
@@ -0,0 +1,12 @@
+package software.amazon.ssm.maintenancewindow;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class CallbackContext {
+
+}
---
+++ aws-ssm-maintenancewindow/src/main/java/software/amazon/ssm/maintenancewindow/Configuration.java
@@ -0,0 +1,12 @@
+package software.amazon.ssm.maintenancewindow;
+
+import java.util.Map;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-ssm-maintenancewindow.json");
+    }
+}
