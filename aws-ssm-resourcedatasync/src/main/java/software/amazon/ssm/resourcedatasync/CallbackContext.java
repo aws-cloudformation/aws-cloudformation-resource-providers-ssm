a68e8a61f20df588f6a74c3932357a90ccd3bdd7
Webb Tseng <wtseng@amazon.com>
2020-04-13 16:44:44 -0700

    Init setup

---
+++ aws-ssm-resourcedatasync/src/main/java/software/amazon/ssm/resourcedatasync/CallbackContext.java
@@ -0,0 +1,12 @@
+package software.amazon.ssm.resourcedatasync;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class CallbackContext {
+
+}
