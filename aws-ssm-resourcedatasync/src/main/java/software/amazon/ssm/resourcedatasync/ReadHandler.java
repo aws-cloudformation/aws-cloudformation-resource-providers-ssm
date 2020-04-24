c1c52ae58a1f01d660641a548ab8235d359a5d71
Webb Tseng <wtseng@amazon.com>
2020-04-23 17:44:53 -0700

    run precommit to fix End of File Formatting

--- aws-ssm-resourcedatasync/src/main/java/software/amazon/ssm/resourcedatasync/ReadHandler.java
+++ aws-ssm-resourcedatasync/src/main/java/software/amazon/ssm/resourcedatasync/ReadHandler.java
@@ -46,4 +46,3 @@ public class ReadHandler extends BaseHandler<CallbackContext> {
                 .build();
     }
 }
-
--- aws-ssm-resourcedatasync/src/test/java/software/amazon/ssm/resourcedatasync/UpdateHandlerTest.java
+++ aws-ssm-resourcedatasync/src/test/java/software/amazon/ssm/resourcedatasync/UpdateHandlerTest.java
@@ -121,4 +121,3 @@ public class UpdateHandlerTest extends TestBase {
         });
     }
 }
-
