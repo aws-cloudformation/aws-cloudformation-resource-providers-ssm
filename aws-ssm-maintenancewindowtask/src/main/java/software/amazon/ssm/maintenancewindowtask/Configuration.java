b114a5e93f1bbe572734effb4c0dd74f3fb1ef66
Leyu Fei <feleyu@amazon.com>
2020-05-11 21:23:17 +0000

    Init resource handlers
    cr https://code.amazon.com/reviews/CR-25212588

---
+++ aws-ssm-maintenancewindowtask/src/main/java/software/amazon/ssm/maintenancewindowtask/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.ssm.maintenancewindowtask;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-ssm-maintenancewindowtask.json");
+    }
+}
