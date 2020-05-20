0ba61452b828765da704addd6e0bb59dd62b957e
Gowtam Maligireddy <gowtamm@amazon.com>
2020-05-20 16:49:19 -0700

    update

--- aws-ssm-document/src/main/java/com/amazonaws/ssm/document/DocumentExceptionTranslator.java
+++ aws-ssm-document/src/main/java/com/amazonaws/ssm/document/DocumentExceptionTranslator.java
@@ -1,34 +1,27 @@
 package com.amazonaws.ssm.document;
 
-import java.util.List;
-import com.google.common.collect.ImmutableList;
-import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.services.ssm.model.AutomationDefinitionNotFoundException;
 import software.amazon.awssdk.services.ssm.model.AutomationDefinitionVersionNotFoundException;
 import software.amazon.awssdk.services.ssm.model.DocumentAlreadyExistsException;
 import software.amazon.awssdk.services.ssm.model.DocumentLimitExceededException;
 import software.amazon.awssdk.services.ssm.model.DocumentVersionLimitExceededException;
-import software.amazon.awssdk.services.ssm.model.DuplicateDocumentContentException;
-import software.amazon.awssdk.services.ssm.model.DuplicateDocumentVersionNameException;
 import software.amazon.awssdk.services.ssm.model.InvalidDocumentContentException;
 import software.amazon.awssdk.services.ssm.model.InvalidDocumentException;
 import software.amazon.awssdk.services.ssm.model.InvalidDocumentSchemaVersionException;
 import software.amazon.awssdk.services.ssm.model.InvalidDocumentVersionException;
 import software.amazon.awssdk.services.ssm.model.MaxDocumentSizeExceededException;
 import software.amazon.awssdk.services.ssm.model.SsmException;
-import software.amazon.cloudformation.exceptions.CfnAccessDeniedException;
 import software.amazon.cloudformation.exceptions.CfnAlreadyExistsException;
 import software.amazon.cloudformation.exceptions.CfnGeneralServiceException;
 import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
 import software.amazon.cloudformation.exceptions.CfnNotFoundException;
 import software.amazon.cloudformation.exceptions.CfnServiceLimitExceededException;
-import software.amazon.cloudformation.exceptions.ResourceAlreadyExistsException;
 
 import lombok.NonNull;
 
 class DocumentExceptionTranslator {
 
-    private static final int GENERIC_VALIDATION_STATUS_CODE = 400;
+    private static final int GENERIC_USER_ERROR_STATUS_CODE = 400;
 
     private static DocumentExceptionTranslator INSTANCE;
 
@@ -59,7 +52,7 @@ class DocumentExceptionTranslator {
 
             return new CfnNotFoundException(ResourceModel.TYPE_NAME, documentName);
 
-        } else if (e.statusCode() == GENERIC_VALIDATION_STATUS_CODE) {
+        } else if (e.statusCode() == GENERIC_USER_ERROR_STATUS_CODE) {
             return new CfnInvalidRequestException(e.getMessage(), e);
         }
 
