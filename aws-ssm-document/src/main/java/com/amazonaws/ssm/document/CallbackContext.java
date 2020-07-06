734197b10ade4298a20cc68f87ed7ee34887798a
Gowtam Maligireddy <gowtamm@amazon.com>
2020-07-06 11:30:01 -0700

    remove ContentAsString attribute and support text content in Content attribute

--- aws-ssm-document/src/main/java/com/amazonaws/ssm/document/CallbackContext.java
+++ aws-ssm-document/src/main/java/com/amazonaws/ssm/document/CallbackContext.java
@@ -2,17 +2,26 @@ package com.amazonaws.ssm.document;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Builder
 @JsonDeserialize(builder = CallbackContext.CallbackContextBuilder.class)
+@EqualsAndHashCode
 public class CallbackContext {
 
+    /**
+     * Boolean that tells whether the create operation on resource started.
+     */
     private Boolean createDocumentStarted;
 
+    /**
+     * Boolean that tells whether the operation on resource started.
+     */
     private Boolean eventStarted;
 
     private Integer stabilizationRetriesRemaining;
@@ -20,5 +29,9 @@ public class CallbackContext {
     @JsonIgnore
     public void decrementStabilizationRetriesRemaining() {
         stabilizationRetriesRemaining--;
+    }
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class CallbackContextBuilder {
     }
 }
