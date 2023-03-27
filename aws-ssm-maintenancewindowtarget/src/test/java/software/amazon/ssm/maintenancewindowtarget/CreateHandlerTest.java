64b2815bb3fed157ce2d03eb78d767618c12db97
Jucai Yan <yanjucai@amazon.com>
2023-03-27 11:15:01 -0700

    Fix ssm maintenancewindowtarget CreateHandler test import

--- aws-ssm-maintenancewindowtarget/src/test/java/software/amazon/ssm/maintenancewindowtarget/CreateHandlerTest.java
+++ aws-ssm-maintenancewindowtarget/src/test/java/software/amazon/ssm/maintenancewindowtarget/CreateHandlerTest.java
@@ -29,8 +29,8 @@ import software.amazon.ssm.maintenancewindowtarget.translator.ExceptionTranslato
 
 import java.util.function.Function;
 
-import static junit.framework.Assert.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
